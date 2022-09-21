c6cf9116528ebae80014a30c799fe20163b52e18
ayayousef96 <ayayou1205@gmail.com>
2022-09-21 15:00:57 +0300

    5.4

---
+++ 5.4/app.js
@@ -0,0 +1,18 @@
+function DarkOrLight(color){
+    let c= color.toLowerCase();
+    switch(c){
+        case 'yellow':
+            return 'light color';
+        case 'pink':
+            return 'light color';
+        case 'orange':
+            return 'light color';
+        case 'blue':
+            return "dark color";
+        case  'purple':
+            return "dark color";
+        case 'brown':
+            return "dark color";
+
+    }  
+}
